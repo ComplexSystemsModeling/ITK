030eeced5020874b0260773ae471451be868b1a9
Alex. Gouaillard <agouaillard@gmail.com>
2011-07-24 11:31:51 +0800

    BUG: fix the evaluate parameter type.

--- Examples/DataRepresentation/Mesh/SimplexMesh.cxx
+++ Examples/DataRepresentation/Mesh/SimplexMesh.cxx
@@ -1,18 +1,24 @@
 #include "itkQuadEdgeMesh.h"
+#include "itkQuadEdgeMeshPoint.h"
 #include "itkRegularSphereMeshSource.h"
 #include "itkDefaultStaticMeshTraits.h"
 #include "itkVTKPolyDataWriter.h"
-#include <iostream>
+#include "itkQuadEdgeMeshBoundaryEdgesMeshFunction.h"
+
+//#include<conio.h>
+#include<iostream>
 
 int main( int argc, char ** argv )
 {
 
   typedef itk::QuadEdgeMesh< float, 3>   MeshType;
+
   typedef MeshType::PointIdentifier      PointIdentifier;
   typedef MeshType::CellIdentifier       CellIdentifier;
   typedef MeshType::PointsContainer      PointsContainer;
   typedef MeshType::CellsContainer       CellsContainer;
   typedef MeshType::CellType             CellType;
+  typedef MeshType::PointIdList          PointIdList;
 
   typedef CellType::PointIdConstIterator PointIdConstIterator;
 
@@ -24,6 +30,8 @@ int main( int argc, char ** argv )
 
   typedef SphereMeshSourceType::PointType   PointType;
   typedef SphereMeshSourceType::VectorType  VectorType;
+
+  typedef itk::QuadEdgeMeshBoundaryEdgesMeshFunction< MeshType > BoundaryLocatorType;
 
   PointType center;
   center.Fill( 0.0 );
@@ -42,7 +50,7 @@ int main( int argc, char ** argv )
     }
   catch( itk::ExceptionObject & excp )
     {
-    std::cerr << "Error during Update() " << std::endl;
+    std::cerr << "Error during Sphere Mesh Update() " << std::endl;
     std::cerr << excp << std::endl;
     return EXIT_FAILURE;
     }
@@ -52,8 +60,7 @@ int main( int argc, char ** argv )
 
   // take care of the primal cells => generate dual points
   const CellsContainer *cells = myMesh->GetCells();
-  std::list< std::pair< CellIdentifier ,PointIdentifier > >   DualPointFromPrimalTriangleLUT;
-  PointsContainer * DualPoints = PointsContainer::New();
+  std::map< CellIdentifier ,PointIdentifier >   DualPointFromPrimalTriangleLUT;
 
   if( cells )
     {
@@ -82,8 +89,8 @@ int main( int argc, char ** argv )
             std::cout << "Found triangle." << std::endl;
 
             // compute dual point coordinate
-            PointIdConstIterator current = cellIterator.Value()->PointIdsBegin();
-            PointIdConstIterator end     = cellIterator.Value()->PointIdsEnd();
+            PointIdConstIterator current= cellIterator.Value()->PointIdsBegin();
+            PointIdConstIterator end    = cellIterator.Value()->PointIdsEnd();
             PointType d_point;
             d_point[0] = 0.0;
             d_point[1] = 0.0;
@@ -91,7 +98,7 @@ int main( int argc, char ** argv )
             while( current != end )
               {
               PointType point = myMesh->GetPoint( *current );
-              std::cout << point << std::endl;
+              //std::cout << point << std::endl;
               for( unsigned int i =0; i < 3; i++ ) d_point[i] += point[i];
               current++;
               }
@@ -99,8 +106,10 @@ int main( int argc, char ** argv )
 
             // push dual point in dualPoints container
             myDualMesh->SetPoint( numberOfDualPoints, d_point );
+            CellIdentifier cellIdentifier = cellIterator.Index();
+            std::cout << "Triangle ID: " << cellIdentifier << std::endl;
+            DualPointFromPrimalTriangleLUT[cellIdentifier] = numberOfDualPoints;
             numberOfDualPoints++;
-
             }
           break;
         case 7: //QUADRATIC_EDGE_CELL:
@@ -114,37 +123,53 @@ int main( int argc, char ** argv )
     }
 
   // take care of the primal points => generate dual cells
+  const PointsContainer *primalPoints = myMesh->GetPoints();
+  const PointsContainer *dualPoints   = myDualMesh->GetPoints();
 
-  const PointsContainer *points = myMesh->GetPoints();
-
-  std::map< PointIdentifier, PointIdentifier > IdMap;
-  PointIdentifier                              k = 0;
-
-  if ( points )
+  if( primalPoints )
     {
-    PointIterator pointIterator = points->Begin();
-    PointIterator pointEnd = points->End();
+    PointIterator pointIterator = primalPoints->Begin();
+    PointIterator pointEnd      = primalPoints->End();
 
-    while ( pointIterator != pointEnd )
+    while( pointIterator != pointEnd )
       {
-      PointType point = pointIterator.Value();
-
-      // outputFile << point[0] << " " << point[1];
-      // outputFile << " " << point[2];
-
-      IdMap[pointIterator.Index()] = k++;
-      pointIterator++;
+      typedef MeshType::QEType QuadEdgeType;
 
       // grab the QEdge
-      // iterate around the o-ring
-      // for each triangle, get the dual point ID
-      // store it in a list of ID,
-      // create the dual cell
-      // add the dual cell, to the dual cell container of the output
+      PointType point = pointIterator.Value();
+      QuadEdgeType * start = point.GetEdge();
+      QuadEdgeType * current = point.GetEdge();
 
+      // create a point ID list to hold the dual point IDs while
+      // we are iterating around a primal point to create the dual cell
+      PointIdList pointidlist;
+
+      // iterate around the o-ring
+      do
+        {
+        // get the id of the face on the left
+        QuadEdgeType::DualOriginRefType leftTriangle;
+        leftTriangle = current->GetLeft();
+
+        // push the dual point ID to the point ID list
+        pointidlist.push_back( DualPointFromPrimalTriangleLUT[ leftTriangle ] );
+
+        current = current->GetOnext();
+
+        } while( current != start );
+
+      // point list is complete, add the dual cell to the dual mesh;
+      myDualMesh->AddFace( pointidlist );
+
+      // next point
+      pointIterator++;
       }
     }
 
+  BoundaryLocatorType::Pointer boundaryEdges = BoundaryLocatorType::New();
+
+  MeshType::EdgeListPointerType boundaryEdgesPointerList;
+  boundaryEdgesPointerList = boundaryEdges->Evaluate( *myMesh );
 
   typedef itk::VTKPolyDataWriter<MeshType>   WriterType;
   WriterType::Pointer writer = WriterType::New();
@@ -155,7 +180,7 @@ int main( int argc, char ** argv )
   writer->SetInput( myDualMesh );
   writer->SetFileName( "MyVeryFirstSimplexMesh.vtk" );
   writer->Write();
-
+  // getch();
   return EXIT_SUCCESS;
 
 }
